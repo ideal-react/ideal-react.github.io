795bc303fd7f9e26c0d6ff5a0025e14a6221a9a5
ustccjw <317713370@qq.com>
2015-09-07 13:04:52 +0800

    add custom style to speech-bubble

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import SpeechBubble from 'speech-bubble'
+import styles from './index.css'
 
 const props = {
 	imageUrl: 'https://avatars0.githubusercontent.com/u/14071114',
 	title: 'ideal',
 	content: 'ideal is a organizations of perfect react component',
 	link: 'https://github.com/ideal-react',
+	styles
 }
 
 React.render(<SpeechBubble {...props} />,
 	document.getElementById('speech-bubble-wrap'))
+
